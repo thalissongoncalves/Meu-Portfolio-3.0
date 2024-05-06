56ac6f02c69418ed3f6d083398edd3921f7ab659
thalissongoncalves <thalissongsilva70@gmail.com>
2024-05-06 20:15:44 -0300

    .

--- my-portfoly/src/App.js
+++ my-portfoly/src/App.js
@@ -1,8 +1,37 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
+import React, { useEffect } from 'react';
+import blackCursor from './assets/black-cursor.png';
+import pointerCursor from './assets/black-cursor-clique.png';
 
 const App = () => {
+  useEffect(() => {
+    // Adicionar cursor personalizado padrão
+    document.body.style.cursor = `url(${blackCursor}), auto`;
+    
+    // Adicionar event listeners para os elementos que devem ter cursor personalizado
+    const pointerElements = document.querySelectorAll('.custom-cursor');
+    pointerElements.forEach(element => {
+      element.addEventListener('mouseenter', () => {
+        // Mudar o cursor para o cursor de ponteiro personalizado
+        document.body.style.cursor = `url(${pointerCursor}), auto`;
+      });
+      element.addEventListener('mouseleave', () => {
+        // Voltar ao cursor personalizado padrão
+        document.body.style.cursor = `url(${blackCursor}), auto`;
+      });
+    });
+
+    // Limpar event listeners quando o componente for desmontado
+    return () => {
+      pointerElements.forEach(element => {
+        element.removeEventListener('mouseenter');
+        element.removeEventListener('mouseleave');
+      });
+    };
+  }, []);
+
   return (
     <div>
       <Header />
