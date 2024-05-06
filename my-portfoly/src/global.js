735f8c0aa4951e0591eeb47584c8e2b6e860a619
thalissongoncalves <thalissongsilva70@gmail.com>
2024-05-06 17:50:07 -0300

    .

--- my-portfoly/src/global.js
+++ my-portfoly/src/global.js
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
-import background from './assets/background.png';
 
 export default createGlobalStyle`
   *, body {
-    background-image: url(${background});
     margin: 0;
     padding: 0;
     scrollbar-width: none;
